d6d14666cf55cc2159e3dd61e2ee85f497c86df0
Barik766 <eobrazcov36@gmail.com>
2024-10-28 17:46:24 +0100

    Working version

---
+++ BookStore/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,119 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.DataAcess.Data;
+using Bulky.Models;
+using Bulky.Models.ViewModels;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BookStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    //[Authorize(Roles = SD.Role_Admin)]
+
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CompanyController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List<Company> objCompanyList = _unitOfWork.Company.GetAll().ToList();
+            
+            return View(objCompanyList);
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            
+            if (id == null || id == 0)
+            {
+                return View(new Company());
+            }
+            else
+            {
+                Company companyObj = _unitOfWork.Company.Get(u=>u.Id==id);
+                return View(companyObj);
+            }
+            
+        }
+
+        [HttpPost]
+        public IActionResult Upsert(Company company)
+        {
+           
+            if (ModelState.IsValid)
+            {
+
+                if (company.Id == 0)
+                {
+                    _unitOfWork.Company.Add(company);
+                }
+                else
+                {
+                    _unitOfWork.Company.Update(company);
+                }
+
+                _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                
+                return View(company);
+            }
+            
+
+        }
+
+        
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Company obj = _unitOfWork.Company.Get(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Company.Remove(obj);
+            _unitOfWork.Save();
+            return RedirectToAction("Index");
+
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll(int id)
+        {
+            List<Company> objProductList = _unitOfWork.Company.GetAll().ToList();
+            return Json(new { data = objProductList });
+
+        }
+
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var companyToBeDeleted = _unitOfWork.Company.Get(u => u.Id == id);
+            if (companyToBeDeleted == null)
+            {
+                return Json(new { success = false, message = "Error :(" });
+
+            }
+
+            _unitOfWork.Company.Remove(companyToBeDeleted);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Success :)" });
+
+        }
+
+        #endregion
+
+    }
+}
---
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,124 @@
+﻿using Bulky.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.DataAcess.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Category___Id = 1, Name = "Action", DisplayOrder=1},
+                new Category { Category___Id = 2, Name = "Sci-Fi", DisplayOrder = 2 },
+                new Category { Category___Id = 3, Name = "History", DisplayOrder = 3 });
+
+            modelBuilder.Entity<Company>().HasData(
+                new Company { Id = 1, Name = "Microsoft", StreetAddress = "1 Microsoft Way", City = "Redmond", State = "WA", PostalCode = "98052", PhoneNumber = "425 882 0000" },
+                new Company { Id = 2, Name = "Apple", StreetAddress = "1 Infinite Loop", City = "Cupertino", State = "CA", PostalCode = "95014", PhoneNumber = "408 996 1000" },
+                new Company { Id = 3, Name = "Google", StreetAddress = "1600 Amphitheatre Parkway", City = "Mountain View", State = "CA", PostalCode = "94043", PhoneNumber = "650 253 0000" }
+);
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Title = "Fortune of Time",
+                    Author = "Billy Spark",
+                    Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+                    ISBN = "SWD9999001",
+                    ListPrice = 99,
+                    Price = 90,
+                    Price50 = 85,
+                    Price100 = 80,
+                    CategoryReferenceId = 1,
+                    ImageUrl=""
+                },
+                new Product
+                {
+                    Id = 2,
+                    Title = "Dark Skies",
+                    Author = "Nancy Hoover",
+                    Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+                    ISBN = "CAW777777701",
+                    ListPrice = 40,
+                    Price = 30,
+                    Price50 = 25,
+                    Price100 = 20,
+                    CategoryReferenceId = 1,
+                    ImageUrl = ""
+                },
+                new Product
+                {
+                    Id = 3,
+                    Title = "Vanish in the Sunset",
+                    Author = "Julian Button",
+                    Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+                    ISBN = "RITO5555501",
+                    ListPrice = 55,
+                    Price = 50,
+                    Price50 = 40,
+                    Price100 = 35,
+                    CategoryReferenceId = 2,
+                    ImageUrl = ""
+                },
+                new Product
+                {
+                    Id = 4,
+                    Title = "Cotton Candy",
+                    Author = "Abby Muscles",
+                    Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+                    ISBN = "WS3333333301",
+                    ListPrice = 70,
+                    Price = 65,
+                    Price50 = 60,
+                    Price100 = 55,
+                    CategoryReferenceId = 2,
+                    ImageUrl = ""
+                },
+                new Product
+                {
+                    Id = 5,
+                    Title = "Rock in the Ocean",
+                    Author = "Ron Parker",
+                    Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+                    ISBN = "SOTJ1111111101",
+                    ListPrice = 30,
+                    Price = 27,
+                    Price50 = 25,
+                    Price100 = 20,
+                    CategoryReferenceId = 3,
+                    ImageUrl = ""
+                },
+                new Product
+                {
+                    Id = 6,
+                    Title = "Leaves and Wonders",
+                    Author = "Laura Phantom",
+                    Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+                    ISBN = "FOT000000001",
+                    ListPrice = 25,
+                    Price = 23,
+                    Price50 = 22,
+                    Price100 = 20,
+                    CategoryReferenceId = 3,
+                    ImageUrl = ""
+                });
+        }
+    }
+}
